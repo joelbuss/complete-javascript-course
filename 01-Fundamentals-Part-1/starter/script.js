6356cb920c65b6c60f0344f82992697926c631cc
joelbuss <joel.buss87@gmail.com>
2021-02-09 07:55:19 -0700

    operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,16 @@
-let js = "amazing";
-    console.log(40 + 8 + 23 -10);
+// let js = "amazing";
+//     console.log(40 + 8 + 23 -10);
 
-    console.log("Joel"); 
-    console.log(18);
+//     console.log("Joel"); 
+//     console.log(18);
 
-    let firstname = "Matilda"; 
+//     let firstname = "Matilda"; 
 
-    console.log(firstname); 
-    console.log(firstname); 
-    console.log(firstname); 
+//     console.log(firstname); 
+//     console.log(firstname); 
+//     console.log(firstname); 
 
-     
+
+const firstName = 'Joel'
+const lastName = 'Buss'
+     console.log(firstName + ' ' + lastName); 
