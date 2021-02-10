c1805634472bbb03ba586b06a698ae472f69b00a
joelbuss <joel.buss87@gmail.com>
2021-02-09 21:17:48 -0700

    basic operators

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,15 +1,31 @@
-let country = 'United States'; 
-let continent = 'North America'; 
-let population = '331.42 Million'; 
+// Lecture: Values and Variables 
+
+const country = 'United States';
+const continent = 'North America'; 
+let population = '331.42 '; 
 
 console.log(country);
 console.log(continent); 
 console.log(population); 
 
-let isIsland = false; 
-let language = 'English'
+// Lecture: Data Types & let, const and var
 
-console.log(isIsland);
-console.log(population)
-console.log(country); 
-console.log(language); 
+const isIsland = false; 
+const language = 'English';
+
+// console.log(typeof isIsland);
+// console.log(typeof population)
+// console.log(typeof country); 
+// console.log(typeof language); 
+
+//Lecture: Basic Operatpors 
+
+console.log(population / 2);
+population++;
+console.log(population > 6); 
+console.log(population > 33); 
+const description1 = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+console.log(description1); 
+
+
+
