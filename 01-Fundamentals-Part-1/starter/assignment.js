fa2976113b0dfed23c871b2cd144ee0b9195ecc7
joelbuss <joel.buss87@gmail.com>
2021-02-11 23:39:35 -0700

    code challenge 3

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -109,3 +109,74 @@ console.log('23' - '10' - 3);
 console.log('23' / '2'); 
 console.log('23' > '18'); 
 */
+
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks �
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+
+// const dolphines = 'average score'; 
+// const koalas = 'average score';
+
+// let dolphine1 = 96; 
+// let dolphine2 = 108; 
+// let dolphine3 = 89;
+
+// let koalas1 = 88; 
+// let koalas2 = 91;
+// let koalas3 = 110;
+
+// const avScoreDolphines = (dolphine1 + dolphine2 + dolphine3) / 3; 
+// console.log(avScoreDolphines); 
+
+// const avScoreKoalas = (koalas1 + koalas2 + koalas3) / 3; 
+// console.log(avScoreKoalas); 
+
+// if (avScoreDolphines > avScoreKoalas){ 
+//     console.log('Dolphines win!!');
+// } else {
+//     console.log('Koalas win!!');
+// };
+
+// BONUS 1
+const dolphines = 'average score'; 
+const koalas = 'average score';
+
+let dolphine1 = 97; 
+let dolphine2 = 112; 
+let dolphine3 = 101;
+
+let koalas1 = 109; 
+let koalas2 = 95;
+let koalas3 = 123;
+
+const avScoreDolphines = (dolphine1 + dolphine2 + dolphine3) / 3; 
+console.log(avScoreDolphines); 
+
+const avScoreKoalas = (koalas1 + koalas2 + koalas3) / 3; 
+console.log(avScoreKoalas); 
+
+if (avScoreDolphines > avScoreKoalas){ 
+    console.log('Dolphines win!!');
+} else {
+    console.log('Koalas win!!');
+};
